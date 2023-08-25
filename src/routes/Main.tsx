a184e9b7c8a5a286103df298bfe55e410458a70a
Amit Dhakal <herojk64@gmail.com>
2023-08-25 23:22:37 +0545

    Updates

--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -1,21 +1,25 @@
-import {createBrowserRouter} from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import ErrorElement from '../components/ErrorElement';
 import Completed from '../components/Completed';
 import Main from '../pages/Main';
 
 const baseroute = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main />,
-      errorElement:<ErrorElement/>,
-      
-    },{
-      path:"/completed",
-        element:<Completed />,
-        errorElement:<ErrorElement/>
-    }
-  ]);
+  {
+    path: "*",
+    element: <Main />,
+    errorElement: <ErrorElement />,
+  },
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <ErrorElement />,
 
-  export {baseroute}
-  
-  
+  }, {
+    path: "/completed",
+    element: <Completed />,
+    errorElement: <ErrorElement />
+  }
+]);
+
+export { baseroute }
+
